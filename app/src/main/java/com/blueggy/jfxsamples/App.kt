1014fcd81103b3f34a20bfae9dbcdee82147530a
zak <lanjingl@outlook.com>
2022-03-08 10:50:06 +0800

    Button demo

--- app/src/main/java/com/blueggy/jfxsamples/App.kt
+++ app/src/main/java/com/blueggy/jfxsamples/App.kt
@@ -1,8 +1,12 @@
 package com.blueggy.jfxsamples
 
+import com.jfoenix.controls.JFXButton
 import javafx.application.Application
+import javafx.geometry.Insets
 import javafx.scene.Scene
-import javafx.scene.layout.AnchorPane
+import javafx.scene.control.Button
+import javafx.scene.layout.FlowPane
+import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 
 
@@ -12,10 +16,34 @@ fun main(args: Array<String>) {
 
 class App : Application() {
     override fun start(ps: Stage) {
+
+        val main = FlowPane()
+        main.vgap = 20.0
+        main.hgap = 20.0
+
+        main.children.add(Button("Java Button"))
+        main.children.add(JFXButton("JFoenix Button"))
+
+        main.children.add(JFXButton("RAISED BUTTON").apply {
+            styleClass.add("button-raised")
+        })
+
+        main.children.add(JFXButton("DISABLED").apply {
+            isDisable = true
+        })
+
+        val pane = StackPane().apply {
+            children.add(main)
+            style = "-fx-background-color:WHITE"
+        }
+        StackPane.setMargin(main, Insets(100.0))
+
         ps.apply {
-            title = "JavaFX and Gradle"
-            scene = Scene(AnchorPane())
-            show()
+            this.title = "JFX Button Demo"
+            this.scene = Scene(pane, 800.0, 200.0).apply {
+                stylesheets.add(App::class.java.getResource("/css/components.css")!!.toExternalForm())
+            }
+            this.show()
         }
     }
 }
--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module jfx.samples.app.main {
     requires javafx.graphics;
     requires kotlin.stdlib;
+    requires com.jfoenix;
+    requires javafx.controls;
 
     exports com.blueggy.jfxsamples;
 }
