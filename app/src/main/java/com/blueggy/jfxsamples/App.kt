9f163df18b3c5524fb7940a80fd0d979b1e4b300
zak <lanjingl@outlook.com>
2022-03-09 10:34:20 +0800

    Button demo

--- app/src/main/java/com/blueggy/jfxsamples/App.kt
+++ app/src/main/java/com/blueggy/jfxsamples/App.kt
@@ -1,6 +1,7 @@
 package com.blueggy.jfxsamples
 
 import com.blueggy.jfxsamples.pages.tabPane
+import com.jfoenix.assets.JFoenixResources
 import com.jfoenix.controls.JFXDecorator
 import javafx.application.Application
 import javafx.scene.Scene
@@ -16,7 +17,11 @@ class App : Application() {
         ps.apply {
             this.title = "JFX Button Demo"
             this.scene = Scene(JFXDecorator(ps, tabPane()), 1000.0, 800.0).apply {
-                stylesheets.add(App::class.java.getResource("/css/components.css")!!.toExternalForm())
+                stylesheets.addAll(
+                    JFoenixResources.load("css/jfoenix-fonts.css").toExternalForm(),
+                    JFoenixResources.load("css/jfoenix-design.css").toExternalForm(),
+                    App::class.java.getResource("/css/components.css")!!.toExternalForm()
+                )
             }
             this.show()
         }
